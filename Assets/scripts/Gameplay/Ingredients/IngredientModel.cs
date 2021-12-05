bcf9d3e67442a41b99d3eda2dcce9f479abe858a
EcoEditor <helena.gri@gmail.com>
2021-12-05 13:59:52 +0200

    implement interface

--- Assets/scripts/Gameplay/Ingredients/IngredientModel.cs
+++ Assets/scripts/Gameplay/Ingredients/IngredientModel.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 namespace SnotSoup.Gameplay.Ingredients {
     [CreateAssetMenu(fileName = "Ingredient Model", menuName = "Snot Soup /Ingredients/IngredientModel")]
     public class IngredientModel: ScriptableObject, IIngredientModel {
-        
+        public string Name { get; set; }
+        public float Vorticity { get; set; }
+        public float Viscosity { get; set; }
+        public int SoupImpactScore { get; set; }
     }
 }

c7f9b275f26329763dd3304538019e10fd629ca8
Lena <Helena.gri@gmail.com>
2021-12-05 14:14:52 +0200

    Added variables to Ingredient model and updated Ingredient View

--- Assets/scripts/Gameplay/Ingredients/IngredientView.cs
+++ Assets/scripts/Gameplay/Ingredients/IngredientView.cs
@@ -1,18 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using SnotSoup.Gameplay.Ingredients;
 using UnityEngine;
 
-public class IngredientView : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class IngredientView : MonoBehaviour {
+    [SerializeField] private IngredientModel model;
 }

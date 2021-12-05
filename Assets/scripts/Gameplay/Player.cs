e2e3a3cdfaecbbf151c8be2291a76790c6251128
Shachar Langbeheim <shachlan@lightricks.com>
2021-12-05 18:40:05 +0200

    Add basic descriptions.

--- Assets/scripts/Gameplay/Player.cs
+++ Assets/scripts/Gameplay/Player.cs
@@ -1,49 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Player : MonoBehaviour
-{
-    [SerializeField] Transform _ingredientParent;
+public class Player : MonoBehaviour {
+  [SerializeField] Transform _ingredientParent;
 
-    private IngredientPicker _target;
-    private bool _canPick = true;
-    private IngredientPicker _pickedItem;
-    private Cauldron _cauldron;
+  private IngredientPicker _target;
+  private IngredientPicker _pickedItem;
+  private Cauldron _cauldron;
 
+  private GameObject _ingredientDescriptionView;
+  private TMP_Text _ingredientDescription;
 
-    public void SetPickupTarget(IngredientPicker target)
-    {
-        _target = target;
+  private void Start() {
+    _ingredientDescriptionView = GameObject.Find("IngredientDescription");
+    _ingredientDescription = _ingredientDescriptionView.transform.Find("Text").GetComponent<TMP_Text>();
+    setIngredientDescription();
+  }
+
+  private void setIngredientDescription() {
+    if (_target == null && _pickedItem == null) {
+      _ingredientDescriptionView.SetActive(false);
+      return;
     }
-
-    public bool CompareTarget(IngredientPicker target)
-    {
-        return target == _target;
+    _ingredientDescriptionView.SetActive(true);
+    if (_target != null && _target != _pickedItem) {
+      _ingredientDescription.text = "Target";
+    } else {
+      _ingredientDescription.text = "picked";
     }
+  }
 
-    public void OnInteract()
-    {
-        if (_target != null && _canPick)
-        {
-            _pickedItem = _target;
-            _pickedItem.BindToTarget(_ingredientParent);
-            _canPick = false;
-        }
-        else if (_target != null)
-        {
-            _pickedItem.Unbind();
-            if (_cauldron != null)
-            {
-                _pickedItem.SetPosition(_cauldron.IngredientDrop);
-            }
-            _pickedItem = null;
-            _canPick = true;
-        }
-    }
+  public void SetPickupTarget(IngredientPicker target) {
+    _target = target;
+    setIngredientDescription();
+  }
 
-    public void SetCauldron(Cauldron drop)
-    {
-        _cauldron = drop;
+  public bool CompareTarget(IngredientPicker target) {
+    return target == _target;
+  }
+
+  private bool canPick() {
+    return _target != null && _pickedItem == null;
+  }
+
+  private bool canDrop() {
+    return _pickedItem != null;
+  }
+
+  public void OnInteract() {
+    if (canPick()) {
+      _pickedItem = _target;
+      _target = null;
+      _pickedItem.BindToTarget(_ingredientParent);
+    } else if (_pickedItem != null) {
+      _pickedItem.Unbind();
+      if (_cauldron != null) {
+        _pickedItem.SetPosition(_cauldron.IngredientDrop);
+        _pickedItem.enabled = false;
+      }
+      _pickedItem = null;
     }
+    setIngredientDescription();
+  }
+
+  public void SetCauldron(Cauldron drop) {
+    _cauldron = drop;
+  }
 }

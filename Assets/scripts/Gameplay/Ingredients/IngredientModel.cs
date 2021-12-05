b77be949c00faca9655b214089becdbe31e6bef8
EcoEditor <helena.gri@gmail.com>
2021-12-05 13:59:09 +0200

    added messaging system and Ingredients code

---
+++ Assets/scripts/Gameplay/Ingredients/IngredientModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace SnotSoup.Gameplay.Ingredients {
+    [CreateAssetMenu(fileName = "Ingredient Model", menuName = "Snot Soup /Ingredients/IngredientModel")]
+    public class IngredientModel: ScriptableObject, IIngredientModel {
+        
+    }
+}
---
+++ Assets/scripts/Utilities/Messages/Messages/MessageBase.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// This class, which is currently empty is a messages base class to reference each message by it
+/// </summary>
+namespace CzernyStudio.Utilities {
+    public class MessageBase { 
+
+    }
+}

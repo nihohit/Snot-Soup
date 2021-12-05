0b77545fdeda7afb6687f51518aa60e3266daf92
itamardag <33083687+itamardag@users.noreply.github.com>
2021-12-05 19:11:11 +0200

    response text

--- Assets/scripts/Gameplay/Cauldron.cs
+++ Assets/scripts/Gameplay/Cauldron.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Cauldron : MonoBehaviour
 {
     [SerializeField] Transform _ingredientDrop;
-    [SerializeField] SnotSoup.BossGameObject _boss;
+    [SerializeField] SnotSoup.BossGameObject _boss; 
+    
+    private GameObject _smallPromptBox;
+    private TMP_Text _descriptionText;
+
+    private Dictionary<SnotSoup.FeedingResponse, string> _responses = new Dictionary<SnotSoup.FeedingResponse, string>() { { SnotSoup.FeedingResponse.Ate, "Looks good!" },
+                                                                                                                           { SnotSoup.FeedingResponse.Refused, "Get that out of my face!" } };
+
     public Vector3 IngredientDrop { get { return _ingredientDrop.position; } }
 
     private List<SnotSoup.Gameplay.Ingredients.IngredientModel> _ingredients = new List<SnotSoup.Gameplay.Ingredients.IngredientModel>();
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
+    {
+        _smallPromptBox = GameObject.Find("SmallPromptBox");
+        _descriptionText = _smallPromptBox.transform.Find("Text").GetComponent<TMP_Text>();
+    }
+
+        private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -37,6 +51,8 @@ public class Cauldron : MonoBehaviour
     {
         var soup = SnotSoup.ResultCalculator.getSoupResult(new SnotSoup.CookingInputs(_ingredients));
         var response = _boss.TryFeed(soup);
+        _smallPromptBox.SetActive(true);
+        _descriptionText.text = _responses[response];
         _ingredients.Clear();
     }
 }

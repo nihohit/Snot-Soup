f91e72aaa060a0e0874d1b0c138ea92e352ecef7
Shachar Langbeheim <shachlan@lightricks.com>
2021-12-05 18:54:23 +0200

    Added descriptions

--- Assets/scripts/Gameplay/IngredientPicker.cs
+++ Assets/scripts/Gameplay/IngredientPicker.cs
@@ -2,51 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IngredientPicker : MonoBehaviour
-{
-    [SerializeField] GameObject _ingredient;
-    
-    private Rigidbody _rb;
+public class IngredientPicker : MonoBehaviour {
+  [SerializeField] GameObject _ingredient;
 
-    protected void Start()
-    {
-        _rb = _ingredient.GetComponent<Rigidbody>();
-    }
+  private Rigidbody _rb;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            var p = other.GetComponent<Player>();
-            p.SetPickupTarget(this);
-        }
-    }
+  protected void Start() {
+    _rb = _ingredient.GetComponent<Rigidbody>();
+  }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            var p = other.GetComponent<Player>();
-            if (p.CompareTarget(this))
-            p.SetPickupTarget(null);
-        }
+  private void OnTriggerEnter(Collider other) {
+    if (!enabled) {
+      return;
     }
+    if (other.gameObject.CompareTag("Player")) {
+      var p = other.GetComponent<Player>();
+      p.SetPickupTarget(this);
+    }
+  }
 
-    public void BindToTarget(Transform target)
-    {
-        _rb.isKinematic = true;
-        _ingredient.transform.parent = target;
-        _ingredient.transform.localPosition = Vector3.zero;
+  private void OnTriggerExit(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      var p = other.GetComponent<Player>();
+      if (p.CompareTarget(this))
+        p.SetPickupTarget(null);
     }
+  }
 
-    public void Unbind()
-    {
-        _rb.isKinematic = false;
-        _ingredient.transform.parent = null;
-    }
+  public void BindToTarget(Transform target) {
+    _rb.isKinematic = true;
+    _ingredient.transform.parent = target;
+    _ingredient.transform.localPosition = Vector3.zero;
+  }
 
-    public void SetPosition(Vector3 pos)
-    {
-        _ingredient.transform.position = pos;
-    }
+  public void Unbind() {
+    _rb.isKinematic = false;
+    _ingredient.transform.parent = null;
+  }
+
+  public void SetPosition(Vector3 pos) {
+    _ingredient.transform.position = pos;
+  }
 }

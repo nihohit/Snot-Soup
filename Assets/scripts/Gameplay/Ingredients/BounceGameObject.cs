b849dce3de8e6374c01872e4a86c901576e697ae
Lena <Helena.gri@gmail.com>
2021-12-06 19:39:16 +0200

    fixed mini ingredients sizes to fit the pot

--- Assets/scripts/Gameplay/Ingredients/BounceGameObject.cs
+++ Assets/scripts/Gameplay/Ingredients/BounceGameObject.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 namespace SnotSoup.Gameplay.Ingredients {
     public class BounceGameObject : MonoBehaviour {
         [SerializeField] private float speed = 4f;
+        [SerializeField] private float heightOffset = 2f;
 
         private bool _isBouncing;
 
@@ -16,7 +17,7 @@ namespace SnotSoup.Gameplay.Ingredients {
         
         protected void Update() {
             if (!_isBouncing) return;
-            var bounce = 0.2f * Mathf.Sin(Time.time * speed) * 0.5f + 1.2f;
+            var bounce = 0.4f * Mathf.Sin(Time.time * speed) * 0.5f + heightOffset;
             transform.position = new Vector3(transform.position.x, bounce, transform.position.z);
         }
     }

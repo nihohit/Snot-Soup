68d0603df3fcf8cf81f9da8e5010f12cbb327a44
Shachar Langbeheim <shachlan@lightricks.com>
2021-12-06 19:23:49 +0200

    Use more food types.

--- Assets/scripts/Utilities/RandomObjectPool.cs
+++ Assets/scripts/Utilities/RandomObjectPool.cs
@@ -2,89 +2,85 @@
 using System.Collections.Generic;
 
 namespace CzernyStudio.Utilities {
-	public class RandomObjectPool : MonoBehaviour {
-		[SerializeField] GameObject[] pooledPrefabs;
+  public class RandomObjectPool : MonoBehaviour {
+    [SerializeField] GameObject[] pooledPrefabs;
 
-		public GameObject[] PooledPrefabs {
-			get {
-				return pooledPrefabs;
-			}
-		}
+    public GameObject[] PooledPrefabs {
+      get {
+        return pooledPrefabs;
+      }
+    }
 
-		private Stack<GameObject> _inactiveInstances = new Stack<GameObject>();
+    private Stack<GameObject> _inactiveInstances = new Stack<GameObject>();
 
-		public GameObject GetObject(int index) {
-			GameObject spawnedGameObject;
+    public GameObject GetObject(int index) {
+      GameObject spawnedGameObject;
 
-			// if there is an inactive instance of the prefab ready to return, return that
-			if (_inactiveInstances.Count > 0) 
-			{
-				// remove the instance from teh collection of inactive instances
-				spawnedGameObject = _inactiveInstances.Pop();
-			}
-			// otherwise, create a new instance
-			else {
-				spawnedGameObject = Instantiate(pooledPrefabs[index]);
+      // if there is an inactive instance of the prefab ready to return, return that
+      if (_inactiveInstances.Count > 0) {
+        // remove the instance from teh collection of inactive instances
+        spawnedGameObject = _inactiveInstances.Pop();
+      }
+      // otherwise, create a new instance
+      else {
+        spawnedGameObject = Instantiate(pooledPrefabs[index]);
 
-				var pooledObject = spawnedGameObject.GetComponent<PooledObject>();
-				pooledObject.pool = this;
-			}
+        var pooledObject = spawnedGameObject.GetComponent<PooledObject>();
+        pooledObject.pool = this;
+      }
 
-			// put the instance in the root of this script
-			spawnedGameObject.transform.SetParent(transform, false);
-			spawnedGameObject.SetActive(true);
+      // put the instance in the root of this script
+      spawnedGameObject.transform.SetParent(transform, false);
+      spawnedGameObject.SetActive(true);
 
-			// return a reference to the instance
-			return spawnedGameObject;
-		}
-		
-		public GameObject GetRandomObject() 
-		{
-			GameObject spawnedGameObject;
+      // return a reference to the instance
+      return spawnedGameObject;
+    }
 
-			// if there is an inactive instance of the prefab ready to return, return that
-			if (_inactiveInstances.Count > 0) 
-			{
-				// remove the instance from teh collection of inactive instances
-				spawnedGameObject = _inactiveInstances.Pop();
-			}
-			// otherwise, create a new instance
-			else {
-				var randomIndex = Random.Range(0, pooledPrefabs.Length);
-				spawnedGameObject = Instantiate(pooledPrefabs[randomIndex]);
+    public GameObject GetRandomObject() {
+      GameObject spawnedGameObject;
 
-				var pooledObject = spawnedGameObject.GetComponent<PooledObject>();
-				pooledObject.pool = this;
-			}
+      // if there is an inactive instance of the prefab ready to return, return that
+      if (_inactiveInstances.Count > 0) {
+        // remove the instance from teh collection of inactive instances
+        spawnedGameObject = _inactiveInstances.Pop();
+      }
+      // otherwise, create a new instance
+      else {
+        var randomIndex = Random.Range(0, pooledPrefabs.Length);
+        spawnedGameObject = Instantiate(pooledPrefabs[randomIndex]);
+        var view = spawnedGameObject.GetComponent<SnotSoup.Gameplay.Ingredients.IngredientView>();
+        view.LoadModel();
 
-			// put the instance in the root of this script
-			spawnedGameObject.transform.SetParent(transform, false);
-			spawnedGameObject.SetActive(true);
+        var pooledObject = spawnedGameObject.GetComponent<PooledObject>();
+        pooledObject.pool = this;
+      }
 
-			// return a reference to the instance
-			return spawnedGameObject;
-		}
+      // put the instance in the root of this script
+      spawnedGameObject.transform.SetParent(transform, false);
+      spawnedGameObject.SetActive(true);
 
-		// Return an instance of the prefab to the pool
-		public void ReturnObject(GameObject toReturn) 
-		{
-			var pooledObject = toReturn.GetComponent<PooledObject>();
+      // return a reference to the instance
+      return spawnedGameObject;
+    }
 
-			// if the instance came from this pool, return it to the pool
-			if(pooledObject != null && pooledObject.pool == this)
-			{
-				// make the instance a child of this and disable it
-				toReturn.SetActive(false);
+    // Return an instance of the prefab to the pool
+    public void ReturnObject(GameObject toReturn) {
+      var pooledObject = toReturn.GetComponent<PooledObject>();
 
-				// add the instance to the collection of inactive instances
-				_inactiveInstances.Push(toReturn);
-			}
-			// otherwise, just destroy it
-			else
-			{
-				Debug.LogWarning(toReturn.name + " was returned to a pool it wasn't spawned from! Destroying.");
-				Destroy(toReturn);
-			}
-		}
-	}
+      // if the instance came from this pool, return it to the pool
+      if (pooledObject != null && pooledObject.pool == this) {
+        // make the instance a child of this and disable it
+        toReturn.SetActive(false);
+
+        // add the instance to the collection of inactive instances
+        _inactiveInstances.Push(toReturn);
+      }
+      // otherwise, just destroy it
+      else {
+        Debug.LogWarning(toReturn.name + " was returned to a pool it wasn't spawned from! Destroying.");
+        Destroy(toReturn);
+      }
+    }
+  }
 }
